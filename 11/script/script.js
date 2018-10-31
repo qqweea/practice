5e2301f3f49fb53f9862d4b215ce330f6ad85967
V R <vscarabee@gmail.com>
2018-10-31 15:15:24 +0200

    stable version

--- 11/script/script.js
+++ 11/script/script.js
@@ -2,13 +2,13 @@ var btnOpenMenu = document.getElementsByClassName("menu__btn--closed")[0];
 var wrapper = document.getElementsByClassName("wrapper")[0];
 var btnCloseMenu = document.getElementsByClassName("menu__btn--open")[0];
 var menu = document.getElementsByClassName("menu")[0];
-var anchors = document.getElementsByClassName("anchor");
+var anchors = document.getElementsByClassName("menu__link");
 
 
 function openMenu() {
     wrapper.style.width = "75vw";
     btnOpenMenu.style.visibility = "hidden";
-    menu.style.left = "75vw";
+    menu.style.width = "25vw";
     btnCloseMenu.style.visibility = "visible";
     btnCloseMenu.style.right = "26vw";
     btnCloseMenu.style.opacity = "1";
@@ -17,7 +17,7 @@ function openMenu() {
 function closeMenu() {
     wrapper.style.width = "100%";
     btnOpenMenu.style.visibility = "visible";
-    menu.style.left = "100vw";
+    menu.style.width = "0";
     btnCloseMenu.style.visibility = "hidden";
     btnCloseMenu.style.right = "15vw";
     btnCloseMenu.style.opacity = "0";
@@ -27,13 +27,13 @@ function updateAnchors() {
     var anchorsLen = anchors.length;
     var newActive = document.getElementById('a_' + location.hash.replace('#',""));
     for (var i = 0; i < anchorsLen; i++) {
-        anchors[i].classList.remove('anchor--active');
+        anchors[i].classList.remove('menu__link--active');
     } 
     if (location.hash == "") {
-        anchors[0].classList.add("anchor--active");
+        anchors[0].classList.add("menu__link--active");
         window.location.hash = 'home';
     } else {
-        newActive.classList.add("anchor--active");
+        newActive.classList.add("menu__link--active");
         window.onhashchange = function() {updateAnchors()};
         window.onhashchange = function() {closeMenu()};
     }
