f7c4f63fddec751efa865a4cf582f307f1d89170
V R <vscarabee@gmail.com>
2018-11-01 12:21:57 +0200

    add more chnges to hw11

--- 11/script/init.js
+++ 11/script/init.js
@@ -10,3 +10,25 @@ var swiper = new Swiper('.slider-2', {
         delay: 5000,
     },
   });
+
+  var clientsSwiper = new Swiper('.slider-3', {
+    loop: true,
+    slidesPerView: 'auto',
+    spaceBetween: 50,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+    autoplay: {
+        delay: 2000,
+    },
+  });
+
+  $('.slider-3').on('mouseenter', function(e){
+    console.log('stop autoplay');
+    clientsSwiper.autoplay.stop();
+  })
+  $('.slider-3').on('mouseleave', function(e){
+    console.log('start autoplay');
+    clientsSwiper.autoplay.start();
+  })
